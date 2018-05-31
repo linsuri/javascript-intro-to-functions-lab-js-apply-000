beed403f68319c62b97170f575c9a812f24b7726
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-31 03:14:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === "I love you, Grandma.") {
     return "I can't hear you!"
   }
-  else
+  else (string === string.toLowerCase) {
+    return "I can't hear you!"
+  }
 }
 
