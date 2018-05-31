7704592df92813d140352b62175b4efc2c2a1a44
Maneepailin Sriuthenchai <maneepailin@gmail.com>
2018-05-31 03:17:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function sayHiToGrandma(string) {
   else if (string === string.toLowerCase()) {
     return "I can't hear you!"
   }
-  else (string === string.toUpperCase()) {
+  else (string === string.toUpperCase()); {
     return "YES INDEED!"
   }  
 }
